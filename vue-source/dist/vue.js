3ebefcd7bcbd941fed614baa4ef4c9848d432ef2
鲍佳玮 <bjw_0627@163.com>
2020-06-14 21:10:18 +0800

    抽离parserHTML逻辑

--- vue-source/dist/vue.js
+++ vue-source/dist/vue.js
@@ -186,11 +186,6 @@
     observer(data); // 观测这个数据
   }
 
-  // 实现模板的编译
-  // 模板编译原理
-  // 1.先把我们的代码转化成ast语法树 (1)parser解析 (正则)
-  // 2.标记静态树 (2) 树得遍历标记markup
-  // 3.通过ast产生的语法树 生成代码 => render函数 codegen
   var ncname = "[a-zA-Z_][\\-\\.0-9_a-zA-Z]*";
   var qnameCapture = "((?:".concat(ncname, "\\:)?").concat(ncname, ")");
   var startTagOpen = new RegExp("^<".concat(qnameCapture)); // 标签开头的正则 捕获的内容是标签名
@@ -324,6 +319,7 @@
     return root;
   }
 
+  // 实现模板的编译
   function compileToFunctions(template) {
     var root = parseHTML(template);
     console.log(root);
---
+++ vue-source/src/compiler/parser-html.js
@@ -0,0 +1,107 @@
+const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`
+const startTagOpen = new RegExp(`^<${qnameCapture}`) // 标签开头的正则 捕获的内容是标签名
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`) // 匹配标签结尾的 </div>
+const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/ // 匹配属性的
+const startTagClose = /^\s*(\/?)>/ // 匹配标签结束的 >
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g
+let root = null // ast语法树的树根
+let currentParent // 标识当前父亲是谁
+let stack = []
+const ELEMENT_TYPE = 1
+const TEXT_TYPE = 3
+
+function createASTElement(tagName, attrs) {
+  return {
+    tag: tagName,
+    type: ELEMENT_TYPE,
+    children: [],
+    attrs,
+    parent: null,
+  }
+}
+function start(tagName, attrs) {
+  // 遇到开始标签 就创建一个ast元素
+  let element = createASTElement(tagName, attrs)
+  if (!root) {
+    root = element
+  }
+  currentParent = element // 把当前元素标记成父ast树
+  stack.push(element) // 将开始标签存放到栈中
+}
+function chars(text) {
+  text = text.replace(/\s/g, '')
+  if (text) {
+    currentParent.children.push({
+      text,
+      type: TEXT_TYPE,
+    })
+  }
+}
+function end(tagName) {
+  let element = stack.pop()
+  currentParent = stack[stack.length - 1]
+  if (currentParent) {
+    element.parent = currentParent
+    currentParent.children.push(element)
+  }
+}
+export function parseHTML(html) {
+  // 不停的去解析html字符串
+  while (html) {
+    let textEnd = html.indexOf('<')
+    if (textEnd === 0) {
+      // 如果当前索引为0 肯定是一个标签 开始标签 结束标签
+      let startTagMatch = parseStartTag()
+      if (startTagMatch) {
+        start(startTagMatch.tagName, startTagMatch.attrs)
+        continue // 如果开始标签匹配完毕后，继续下一次匹配
+      }
+      let endTagMatch = html.match(endTag)
+      if (endTagMatch) {
+        advance(endTagMatch[0].length)
+        end(endTagMatch[1])
+        continue
+      }
+    }
+    let text
+    if (textEnd >= 0) {
+      text = html.substring(0, textEnd)
+    }
+    if (text) {
+      advance(text.length)
+      chars(text)
+    }
+  }
+  function advance(n) {
+    html = html.substring(n)
+  }
+  function parseStartTag() {
+    let start = html.match(startTagOpen)
+    if (start) {
+      const match = {
+        tagName: start[1],
+        attrs: [],
+      }
+      advance(start[0].length) // 将标签删除
+      let end, attr
+      while (
+        !(end = html.match(startTagClose)) &&
+        (attr = html.match(attribute))
+      ) {
+        advance(attr[0].length) // 将属性删除
+        match.attrs.push({
+          // 解析属性
+          name: attr[1],
+          value: attr[3] || attr[4] || attr[5],
+        })
+      }
+      if (end) {
+        // 去除结束标签，前进一格并返回ast语法树
+        advance(end[0].length)
+        return match
+      }
+    }
+  }
+  return root
+}
